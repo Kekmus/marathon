12117e4f97b5c2b6dcfa0b8d1b12402a99005a1d
Kekmus <lgstrwrs@gmail.com>
2022-03-23 15:41:42 +0500

    feat: add country guesser

--- GetSex/index.js
+++ GetSex/index.js
@@ -1,5 +1,4 @@
 const btn = document.querySelector('.btn')
-let name = document.querySelector('.name');
 
 btn.addEventListener('click', kek)
 
@@ -8,11 +7,17 @@ function sendRequest(url) {
 }
 
 function kek() {
-    if (name.value.length > 0) {
-        const serverUrl = 'https://api.genderize.io';
-        const url = `${serverUrl}?name=${name.value}`;
+    const name = document.querySelector('.name').value
+    if (name.length > 0) {
+        const serverUrlName = 'https://api.genderize.io';
+        let url = `${serverUrlName}?name=${name}`;
         sendRequest(url)
                 .then(data => console.log(`${data.name} is ${data.gender} with probability ${data.probability}`))
+                .catch(err => console.log(err))
+        const serverUrlCountry = 'https://api.nationalize.io';
+        url = `${serverUrlCountry}?name=${name}`;
+        sendRequest(url)
+                .then(data => console.log(`${name} from ${data.country[0].country_id}`))
                 .catch(err => console.log(err))
     } else {
         console.log('Введи имя')
